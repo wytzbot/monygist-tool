502ee369806ccb9966aa58440d84d3439af8f7ff
wytzbot <ilemobayotolulope11092003@gmail.com>
2026-07-01 19:02:24 +0100

    Implement service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+// MonyGist Subscription Manager
+// Service Worker v1
+
+const CACHE_NAME = "monygist-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./manager.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
